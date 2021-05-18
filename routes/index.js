41da6a5a66def9cdbb44cf37e229bd500980e217
Isaiah <computerblender@gmail.com>
2021-05-17 23:36:09 -0700

    Render messages in ejs; add nodemon

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ const messages = [
 ];
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Express", messages });
 });
 
 module.exports = router;
